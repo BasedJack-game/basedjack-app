f4cf74b04aa3a1725aeb6fa6c57d340e85d2b5b2
sarvagnakadiya <sarvagnakadiya1@gmail.com>
2024-06-21 15:40:44 +0530

    removed apis

--- src/app/api/game/deal/route.tsx
+++
@@ -1,8 +0,0 @@
-import { NextResponse } from "next/server";
-import { GameState, Card } from "../../../types/start";
-
-let gameState: GameState = {} as GameState;
-
-export const GET = () => {
-  return NextResponse.json(gameState, { status: 200 });
-};
