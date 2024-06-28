192e6fcedcafb52f2ed82ab95b97258f9fd17df8
bhumi18 <bhumisadariya18@gmail.com>
2024-06-28 11:08:26 +0530

    getUserStats changes

--- src/app/api/getUserStats/route.tsx
+++ src/app/api/getUserStats/route.tsx
@@ -1,25 +1,25 @@
-import { NextResponse } from "next/server";
-import { getUserStats } from "@/app/utils/mongodb";
+// import { NextResponse } from "next/server";
+// import { getUserStats } from "@/app/utils/mongodb";
 
-export async function POST(request: Request): Promise<Response> {
-  try {
-    const body = await request.json();
-    const { address } = body;
+// export async function POST(request: Request): Promise<Response> {
+//   try {
+//     const body = await request.json();
+//     const { address } = body;
 
-    if (!address) {
-      return NextResponse.json(
-        { error: "Address is required" },
-        { status: 400 }
-      );
-    }
+//     if (!address) {
+//       return NextResponse.json(
+//         { error: "Address is required" },
+//         { status: 400 }
+//       );
+//     }
 
-    const userStats = await getUserStats(address);
-    return NextResponse.json({ data: userStats }, { status: 200 });
-  } catch (error) {
-    console.error("Error fetching user stats:", error);
-    return NextResponse.json(
-      { error: "Unable to fetch user stats" },
-      { status: 500 }
-    );
-  }
-}
+//     const userStats = await getUserStats(address);
+//     return NextResponse.json({ data: userStats }, { status: 200 });
+//   } catch (error) {
+//     console.error("Error fetching user stats:", error);
+//     return NextResponse.json(
+//       { error: "Unable to fetch user stats" },
+//       { status: 500 }
+//     );
+//   }
+// }
