8b507ff335c5df830e8776a11ee2e6fb43ecdae1
sarvagnakadiya <sarvagnakadiya1@gmail.com>
2024-06-29 16:47:39 +0530

    live link changed to prod

--- src/app/api/hit/route.tsx
+++ src/app/api/hit/route.tsx
@@ -249,7 +249,7 @@ const handleHit = async (address: string, game: any, collection: any) => {
               {
                 label: `Cast Game`,
                 action: "link",
-                target: `https://warpcast.com/~/compose?text=%F0%9F%8E%89%F0%9F%94%A5+Check+out+this+Nounish+BasedJack+game%2C+a+classic+blackjack+game+on+Farcaster+Frames!+Developed+during+the+On+Chain+Summer+Hackathon+by+Base.+%23based+%23nounish+%23blackjack+%23basedJack+%F0%9F%83%8F%E2%9C%A8&embeds%5B%5D=https://blackjack-next.vercel.app/`,
+                target: `https://warpcast.com/~/compose?text=%F0%9F%8E%89%F0%9F%94%A5+Check+out+this+Nounish+BasedJack+game%2C+a+classic+blackjack+game+on+Farcaster+Frames!+Developed+during+the+On+Chain+Summer+Hackathon+by+Base.+%23based+%23nounish+%23blackjack+%23basedJack+%F0%9F%83%8F%E2%9C%A8&embeds%5B%5D=https://basedjack-next.vercel.app/`,
               },
               {
                 label: `Share Result`,
